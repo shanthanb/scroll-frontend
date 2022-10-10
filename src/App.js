e5495f1e75aff12c8bae072c50b4d5b670b1df85
shanthanb <shanthanboppudi@gmail.com>
2022-10-11 00:38:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import './App.css';
+import img from './image1.jpeg'
+
+const App = () => {
+
+  const [article, setArticle] = useState([])
+  const [pagination, setPagination] = useState(1);
+
+  useEffect((e) => {
+    fetchArticle();
+    //  document.body.onscroll = (e) => {
+    //   console.log(document.body.scrollTop);
+    // }
+
+  }, [])
+
+  const scroll = (e) => {
+
+    console.log(e.target.scrollTop);
+    if (e.target.scrollTop > (e.target.scrollHeight - 800)) {
+      if (pagination > 3) {
+        return
+      }
+      fetchArticle();
+
+    }
+
+    console.log(e.target.scrollTop, e.target.scrollHeight - 800);
+  }
+
+
+
+  const fetchArticle = async () => {
+
+
+    const request = await fetch(`http://localhost:3001/app-api/v1/photo-gallery-feed-page/page/${pagination}`);
+
+    const response = await request.json();
+
+    setArticle([...article, ...response.nodes]); //Updating articles.
+    setPagination(pagination + 1);
+
+
+  }
+
+
+  return (
+    <div className='header'>
+
+      <h1>ARTICLES INFINITE SCROLL</h1>
+      <div className='card-container' onScroll={scroll}>
+        <div className="wrap" style={{ height: "max-content" }}>
+          {article.map((article) => {
+            return (<div className='card-body'>
+              <div className='card-media'>
+                <img src={article.node.field_photo_image_section} />
+
+              </div>
+              <div className='card-content'>
+                <p> {article.node.title}</p>
+                <div>{article.node.path}</div>
+                {/* <div>{new Date().toString()}</div> */}
+              </div>
+            </div>
+            )
+          })
+
+
+          }
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
